b04b9dc3f0e9487e2e9837ae2a862d4136fc67ec
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-06 22:11:57 +0530

    Added props

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,19 +1,22 @@
-function ProductCard() {
+/* eslint-disable react/prop-types */
 
+function ProductCard(props) {
+console.log(props.data)
+const {title,subtitle,price,imgurl,id} = props.data
    
     return ( 
 
 
         <>
-            <div className="card-container w-productwidth h-productheight m-2 font-Inter text-navblue relative" >
-                <img src="https://cdn.shopify.com/s/files/1/0974/7950/products/Everyday-Bundle-Mocks-final_1920x1281_crop_center.jpg?v=1660013247" alt="" />
+            <div  id ={id} className="card-container w-productwidth h-productheight m-2 font-Inter text-navblue relative" >
+                <img src={imgurl} alt="" />
                <div className="product-content flex  items-center justify-between">
                <div className="product-details py-2 my-2">
-                <p className="font-semibold">MINI CONSOLE</p>
-                <p className="text-sm">Ideal for everyday shortcuts. Adaptable to any workflow.</p>
+                <p className="font-semibold">{title}</p>
+                <p className="text-sm">{subtitle}</p>
                 </div>
                 <div className="product-price m-2">
-                    <p>$249</p>
+                    <p>${price}</p>
                 </div>
                </div>
                 <div className="preorder bg-pink1 text-blacktext w-32 py-1 text-center font-bold text-sm absolute top-4 right-0">
--- src/ProductDisplay.jsx
+++ src/ProductDisplay.jsx
@@ -1,16 +1,49 @@
 import ProductCard from "./ProductCard";
 
+const productDetails = [
+    {
+        id: 1,
+        title: "MINI CONSOLE",
+        subtitle: "Ideal for everyday shortcuts. Adaptable to any workflow.",
+        price: 249,
+        imgurl: "https://cdn.shopify.com/s/files/1/0974/7950/products/Everyday-Bundle-Mocks-final_1920x1281_crop_center.jpg?v=1660013247"
+    },
+    {
+        id: 2,
+        title: "AUDIO CONSOLE",
+        subtitle: "Ideal for music & audio. Adaptable to any workflow.",
+        price: 499,
+        imgurl: "https://cdn.shopify.com/s/files/1/0974/7950/products/Packaging_TravelerClosedBracket1_Monogram_May20204517_d3899632-1a43-425f-a16a-1d5b968810c7_750x500_crop_center.jpg?v=1623682377"
+    },
+    {
+        id: 3,
+        title: "PHOTO CONSOLE",
+        subtitle: "Ideal for photo & video. Adaptable to any workflow.",
+        price: 599,
+        imgurl: "https://cdn.shopify.com/s/files/1/0974/7950/products/Packaging_StudioClosedBracket1_Monogram_May20204507_548446cb-17c9-4fc2-8b12-ae1b1a308a7e_1080x720_crop_center.jpg?v=1623269261"
+    },
+    {
+        id: 4,
+        title: "VIDEO CONSOLE",
+        subtitle: "Ideal for Video & Film. Adaptable to any workflow.",
+        price: 899,
+        imgurl: "https://cdn.shopify.com/s/files/1/0974/7950/products/Packaging_MasterClosedBracket1_Monogram_May20204494_1080x720_crop_center.jpg?v=1623682600"
+    }
+]
+
 function ProductDisplay() {
-    return ( 
+    return (
         <>
             <div className="product-display w-10/12 mx-auto  grid grid-cols-2 items-center place-items-center gap-4 my-12 p-4">
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+
+
+                {productDetails.map((object) => (
+                    <ProductCard key={object.id} data={object} />
+                ))}
+
             </div>
         </>
-     );
+    );
 }
 
 export default ProductDisplay;
